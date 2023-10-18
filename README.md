2219357ae82d0ef8bd62c6ed740b8f48ba4343a5
Aqu1tain <corentinfox08@gmail.com>
2023-10-18 10:16:04 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# CvChess
-Python Bot that Plays Chess
-Le bot n'est pas encore implémenté
-Actuellement il y a uniquement le plateau de jeu, le système de tour à tour, et les échecs au roi.
+# Python Discord Bot
+
+This is a starting point for making your own Discord bot using Python and the [discordpy](https://discordpy.readthedocs.io/) library.
+Read [their getting-started guides](https://discordpy.readthedocs.io/en/stable/#getting-started) to get the most out of this template.
+
+## Getting Started
+
+To get set up, you'll need to follow [these bot account setup instructions](https://discordpy.readthedocs.io/en/stable/discord.html),
+and then copy the token for your bot and added it as a secret with the key of `TOKEN` in the "Secrets (Environment variables)" panel.
+
+## FAQ
+
+If you get the following error message while trying to start the server: `429 Too Many Requests` (accompanied by a lot of HTML code), 
+try the advice given in this Stackoverflow question:
+https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests
--- main.py
+++ main.py
@@ -1,89 +1,199 @@
-import pygame
-import pieces
-import board
+import discord
+from discord.ext import commands
+from datetime import datetime, date, timedelta
+from icalendar import Calendar
+from config import TOKEN, CHANNEL_ID
+from keep_alive import keep_alive
+import csv
 
-class ChessGame:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((800, 800))
-        pygame.display.set_caption("Chess Game")
-        self.images = {}
-        self.turn = "White"
-        self.selected_piece = None
-        self.chess_board = board.Chess_Board()
-        self.create_board()
+keep_alive(
+)  # Fonction qui appelle UptimeRobot, pour forcer le Bot à être online h24
 
-    def create_board(self):
-        size = int(100 * 0.9)
-        for row in range(8):
-            for col in range(8):
-                x0 = col * 100
-                y0 = row * 100
-                x1 = x0 + 100
-                y1 = y0 + 100
-                color = (240, 217, 181) if (row + col) % 2 == 0 else (181, 136, 99)
-                pygame.draw.rect(self.screen, color, (x0, y0, x1, y1))
+intents = discord.Intents.default()
+intents.message_content = True
 
-        for pos, piece in self.chess_board.board_positions.items():
-            if piece == '.':
-                continue
-            row, col = 8 - int(pos[1]), ord(pos[0]) - ord('a')
-            x, y = col * 100 + 50 - size // 2, row * 100 + 50 - size // 2
-            self.screen.blit(self.get_image(piece), (x, y))
-            
-        if self.selected_piece:
-            moves = self.chess_board.get_valid_moves(self.selected_piece)
-            for move in moves:
-                row, col = 8 - int(move[1][1]), ord(move[1][0]) - ord('a')
-                x, y = col * 100 + 50 , row * 100 + 50 
-                pygame.draw.circle(self.screen, (130,151,105), (x, y), 15)
+client = commands.Bot(command_prefix='$', intents=intents, help_command=None)
 
 
-        pygame.display.update()
-
-    def get_image(self, piece):
-        symbol = piece.symbol
-        color = piece.color[0].lower()
-        filename = f"Images/{color}{symbol}.png"
-        if filename in self.images:
-            return self.images[filename]
-
-        image = pygame.image.load(filename)
-        size = int(100 * 0.9) # 90% de la taille de la case
-        image = pygame.transform.scale(image, (size, size))
-        self.images[filename] = image
-        return image
-
-    def run(self):
-        running = True
-        self.selected_piece = None
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.MOUSEBUTTONUP:
-                    x, y = pygame.mouse.get_pos()
-                    row, col = y // 100, x // 100
-                    pos = chr(col + ord('a')) + str(8 - row)
-                    piece = self.chess_board.get_piece_from_pos(pos)
-                    if piece and piece != '.' and not self.selected_piece:
-                        if piece.color == self.turn:
-                            self.selected_piece = pos
-                            self.create_board()
-                    elif self.selected_piece:
-                        if self.chess_board.is_valid_move(self.selected_piece, pos):
-                            self.chess_board.move(self.selected_piece, pos)
-                            self.selected_piece = None
-                            if self.turn == "White":
-                                self.turn = "Black"
-                            else:
-                                self.turn = "White"
-                            self.create_board()
-                        else:
-                            self.selected_piece = None
-        pygame.quit()
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}'
+        )  # Afficher quand le bot est prêt à être utilisé
 
 
-if __name__ == "__main__":
-    game = ChessGame()
-    game.run()
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return  # Éviter que le bot se réponde à lui-même
+
+  await client.process_commands(message)
+
+
+@client.command(name="date", description="Vous renvoie la date")
+async def datecommand(ctx):
+  todaydate = date.today()
+  d1 = todaydate.strftime("%d/%m/%Y")
+  await ctx.send('Nous sommes le ' + str(d1))
+
+
+@client.command(
+    name="medias",
+    description="Vous renvoie tous les médias conseillés pendant l\'année")
+async def mediacommand(ctx):
+  embedVar = discord.Embed(
+      title="Medias conseillés",
+      description="Vous renvoie tous les médias conseillés pendant l\'année",
+      color=0x3853B4)
+  with open('CSV_Files/medias.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+      for film in row:
+        embedVar.add_field(name='',
+                           value='**' + str(film) + '**',
+                           inline=False)
+
+  await ctx.send(embed=embedVar)
+
+
+@client.command(
+    name="addmedia",
+    description="Vous renvoie tous les médias conseillés pendant l\'année")
+async def addmediacommand(ctx, nom='NO MEDIA'):
+  embedVar = discord.Embed(title="Ajouter un media",
+                           description="Vous Avez ajouté le media :" + nom,
+                           color=0x3853B4)
+  if nom != 'NO MEDIA':
+    with open('CSV_Files/medias.csv', a, newline='') as csvfile:
+      writer = csv.writer(csvfile, delimiter=',')
+      writer.writerow([str(nom)])
+    ctx.send(embedVar)
+  else:
+    return
+
+
+@client.command(name="cours",
+                description="Affiche les cours pour aujourd'hui ou demain")
+async def cours_command(ctx, jour="today"):
+  # Variables
+  courses = []
+  horaires = []
+  locations = []
+
+  e = open('Agenda/groupeD.ics', 'rb')
+  ecal = Calendar.from_ical(e.read())  # Récupérer le calendrier
+
+  # Si l'utilisateur demande les cours pour "aujourd'hui"
+  if jour.lower() == "today":
+    target_date = date.today()
+  # Si l'utilisateur demande les cours pour "demain"
+  elif jour.lower() == "demain":
+    target_date = date.today() + timedelta(days=1)
+  # Si l'utilisateur fournit une date au format jj/mm/aaaa
+  else:
+    try:
+      target_date = datetime.strptime(jour, "%d/%m/%Y").date()
+    except ValueError:
+      embedVar = discord.Embed(
+          title="Erreur de format",
+          description=
+          "Format de date invalide. Utilisez '**today**', '**demain**' ou une date au format '**jj/mm/aaaa**'.",
+          color=0x3853B4)
+      await ctx.send(embed=embedVar)
+
+  for event in ecal.walk('VEVENT'):
+    Aevent = event.get("DTSTART")
+    step = Aevent.dt.date()
+    horaire = Aevent.dt.strftime("%Hh%M")
+    location = event.get("LOCATION")
+
+    if step == target_date:
+      courses.append(event.get("SUMMARY"))
+      horaires.append(horaire)
+      locations.append(location)
+
+  e.close()
+
+  if len(courses) == 0:
+    embedVar = discord.Embed(
+        title="Aucun cours",
+        description=
+        f"Aucun cours prévu pour le {target_date.strftime('%d/%m/%Y')}.",
+        color=0x3853B4)
+    await ctx.send(embed=embedVar)
+  else:
+    embedVar = discord.Embed(
+        title=f"Emploi du temps {target_date.strftime('%d/%m/%Y')}",
+        description=f"Le {target_date.strftime('%d/%m/%Y')}, vous avez : ",
+        color=0x3853B4)
+    for i in range(min(len(courses), 2)):
+      embedVar.add_field(name=courses[i],
+                         value=f"{horaires[i]} en **{locations[i].lower()}**",
+                         inline=False)
+    await ctx.send(embed=embedVar)
+
+
+@client.command(name="help", description="Affiche les commandes disponibles")
+async def help_command(ctx):
+  embedVar = discord.Embed(title="Aide pour les commandes",
+                           description="Commandes",
+                           color=0x3853B4)
+  embedVar.add_field(name="$help",
+                     value="Demander de l'aide pour les commandes",
+                     inline=False)
+  embedVar.add_field(name='$date',
+                     value='Demander la date du jour',
+                     inline=False)
+  embedVar.add_field(
+      name='$cours [jour]',
+      value=
+      'Afficher les cours pour aujourd\'hui, demain ou une date spécifique',
+      inline=False)
+  embedVar.add_field(name='$medias',
+                     value='Obtenir les médias conseillés existants',
+                     inline=False)
+  embedVar.add_field(name='$addmedia',
+                     value='Ajouter un média conseillé',
+                     inline=False)
+  await ctx.send(embed=embedVar)
+
+
+@client.command(name="ping",
+                description="Obtenir ou perdre le rôle Notifs Agenda")
+async def ping_command(ctx):
+  role_id = 1156174631959531600  # Convertir en int si ce n'est pas déjà le cas
+  member = ctx.author
+  role = discord.utils.get(ctx.guild.roles, id=role_id)
+
+  if role:
+    if role in member.roles:
+      await member.remove_roles(role)
+      embedVar = discord.Embed(
+          title="Rôle",
+          description="Le rôle **Notifs Agenda** a bien été enlevé.",
+          color=0x3853B4)
+      await ctx.send(embed=embedVar)
+    else:
+      await member.add_roles(role)
+      embedVar = discord.Embed(
+          title="Rôle",
+          description="Le rôle **Notifs Agenda** a bien été ajouté.",
+          color=0x3853B4)
+      await ctx.send(embed=embedVar)
+  else:
+    await ctx.send("Le rôle avec l'ID spécifié n'a pas été trouvé.")
+
+
+# Easter egg, si vous êtes arrivés ici, félicitations à vous
+@client.event
+async def on_message(message):
+  await client.process_commands(message)
+  if message.content.startswith('42'):
+    embedVar = discord.Embed(
+        title="42",
+        description=
+        "La solution à la grande question sur la vie, l'univers, et le reste, assurément...",
+        color=0x3853B4)
+    await message.channel.send(embed=embedVar)
+
+
+client.run(TOKEN)
